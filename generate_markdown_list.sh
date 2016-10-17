f2afce8dce82b5345f4397991feb3a01b045ae3c
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2016-10-17 11:23:06 +0200

    Añadido script para mostrar lista formateada

---
+++ generate_markdown_list.sh
@@ -0,0 +1,6 @@
+
+while read -r line;
+do echo -n "[$line](STLs/$line.stl) ";
+#do echo '=HYPERLINK("https://raw.githubusercontent.com/CarlosGS/RetoCloneWarsAperiodico/master/STLs/'$line'.stl", "'$line'")';
+done
+
